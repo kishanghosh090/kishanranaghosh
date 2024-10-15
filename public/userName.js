54ae1e7cc0ce1993c5a05047568e26dd35360052
kishanghsoh090 <kishanghosh090@gmail.com>
2024-10-15 21:42:53 +0530

    file added

--- public/userName.js
+++ public/userName.js
@@ -1,47 +1,60 @@
+/** @format */
+
 // user name input start
 
-document.addEventListener('DOMContentLoaded', function () {
-const userNameContainer = document.querySelector('.userName')
-const usernameinput = document.querySelector("#username-input")
-const userNamebtn = document.querySelector(".userName-btn")
-const DynamicName = document.querySelector(".DynamicName")
-const contactform = document.querySelector(".contact-form")
+document.addEventListener("DOMContentLoaded", function () {
+  const userNameContainer = document.querySelector(".userName");
+  const usernameinput = document.querySelector("#username-input");
+  const userNamebtn = document.querySelector(".userName-btn");
+  const DynamicName = document.querySelector(".DynamicName");
+  const contactform = document.querySelector(".contact-form");
+  const resetUserNamebtn = document.querySelector(".resetUserName-btn");
+  if (JSON.parse(localStorage.getItem("portfolio")) !== null) {
+    document.querySelector(".userName").style.display = "none";
+    document.querySelector(".welcomeMsg").classList.add("welcomeAnimation");
+  }
+  userNameContainer.classList.remove("hidden"); // unhide the user name container
+  let userName = JSON.parse(localStorage.getItem("portfolio")) || [];
+  userName.forEach((name) => {
+    renderTasks(name);
+  });
 
-if (JSON.parse(localStorage.getItem('portfolio')) !== null) {
-    document.querySelector('.userName').style.display = 'none'
-    document.querySelector('.welcomeMsg').classList.add('welcomeAnimation')
-}
-userNameContainer.classList.remove('hidden')// unhide the user name container
-let userName = JSON.parse(localStorage.getItem('portfolio')) || []
-userName.forEach((name) => {
-    renderTasks(name)
-})
-
-userNamebtn.addEventListener('click', () => {
-    const nameText = usernameinput.value.trim()
+  userNamebtn.addEventListener("click", () => {
+    const nameText = usernameinput.value.trim();
     if (nameText === "") return;
     const name = {
-        id: Date.now(),
-        name: nameText,
-        completed: false,
-    }
-    userName.push(name)
-    saveTasks()
-    renderTasks(name)
-    document.querySelector('.userName').classList.add('userName-hide')
-    document.querySelector('.welcomeMsg').classList.add('welcomeAnimation')
-})
+      id: Date.now(),
+      name: nameText,
+      completed: false,
+    };
+    userName.push(name);
+    saveTasks();
+    renderTasks(name);
+    usernameinput.value = "";
+    document.querySelector(".userName").classList.add("userName-hide");
+    document.querySelector(".welcomeMsg").classList.add("welcomeAnimation");
+  });
 
-function renderTasks(name) {
-    console.log(name.name)
-    DynamicName.innerText = name.name
-    contactform.value = name.name.toUpperCase()
-}
+  function renderTasks(name) {
+    console.log(name.name);
+    DynamicName.innerText = name.name;
+    contactform.value = name.name.toUpperCase();
+  }
 
+  function saveTasks() {
+    localStorage.setItem("portfolio", JSON.stringify(userName));
+  }
 
-function saveTasks() {
-    localStorage.setItem('portfolio', JSON.stringify(userName))
-}
-
-})
+  // reset user name
+  resetUserNamebtn.addEventListener("click", () => {
+    resetUserName();
+  });
+  //reset user name
+  function resetUserName() {
+    localStorage.removeItem("portfolio");
+     contactform.value = ''
+    document.querySelector(".userName").classList.remove("userName-hide");
+    document.querySelector(".welcomeMsg").classList.remove("welcomeAnimation");
+  }
+});
 // user name innput end
