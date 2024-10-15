0709d37f857162bd9a7bd2e9fa4be6fd14b4528f
kishanghsoh090 <kishanghosh090@gmail.com>
2024-10-16 00:16:21 +0530

    file added

--- public/userName.js
+++ public/userName.js
@@ -3,16 +3,35 @@
 // user name input start
 
 document.addEventListener("DOMContentLoaded", function () {
+  const coverhide = document.querySelector(".cover-hide");
+  const coverhide2 = document.querySelector(".cover-hide2");
+  const hide = document.querySelector(".hide");
+  const hamani = document.querySelector(".ham-ani");
+  const divrotate1 = document.querySelector(".div-rotate1");
+  const divrotate2 = document.querySelector(".div-rotate2 ");
   const userNameContainer = document.querySelector(".userName");
   const usernameinput = document.querySelector("#username-input");
   const userNamebtn = document.querySelector(".userName-btn");
   const DynamicName = document.querySelector(".DynamicName");
   const contactform = document.querySelector(".contact-form");
-  const resetUserNamebtn = document.querySelector(".resetUserName-btn");
+  const skipButton = document.querySelector(".skip");
+
+  skipButton.addEventListener("click", () => {
+    document.querySelector(".userName").classList.add("userName-hide");
+    document.querySelector(".welcomeMsg").classList.add("welcomeAnimation");
+  });
+
+  const resetUserNamebtn1 = document.querySelector(".resetUserName-btn1");
+  const resetUserNamebtn2 = document.querySelector(".resetUserName-btn2");
+
   if (JSON.parse(localStorage.getItem("portfolio")) !== null) {
+    document.querySelector(".reset-btn-nav").classList.remove("hidden");
+
+    document.querySelector(".resetUserName").classList.remove("hidden");
     document.querySelector(".userName").classList.add("userName-hide");
     document.querySelector(".welcomeMsg").classList.add("welcomeAnimation");
   }
+
   userNameContainer.classList.remove("hidden"); // unhide the user name container
   let userName = JSON.parse(localStorage.getItem("portfolio")) || [];
   userName.forEach((name) => {
@@ -31,6 +50,8 @@ document.addEventListener("DOMContentLoaded", function () {
     saveTasks();
     renderTasks(name);
     usernameinput.value = "";
+    document.querySelector(".reset-btn-nav").classList.remove("hidden");
+    document.querySelector(".resetUserName").classList.remove("hidden");
     document.querySelector(".userName").classList.add("userName-hide");
     document.querySelector(".welcomeMsg").classList.add("welcomeAnimation");
   });
@@ -46,14 +67,26 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // reset user name
-  resetUserNamebtn.addEventListener("click", () => {
+  resetUserNamebtn1.addEventListener("click", () => {
+    coverhide.classList.toggle("cover-hide");
+    hide.classList.toggle("hide");
+    coverhide2.classList.toggle("cover-hide2");
+    hamani.classList.toggle("ham-ani");
+    divrotate1.classList.toggle("div-rotate1");
+    divrotate2.classList.toggle("div-rotate2");
+    resetUserName();
+  });
+  resetUserNamebtn2.addEventListener("click", () => {
     resetUserName();
   });
   //reset user name
   function resetUserName() {
     document.querySelector(".userName").classList.remove("userName-hide");
     document.querySelector(".welcomeMsg").classList.remove("welcomeAnimation");
+    DynamicName.innerText = "";
     contactform.value = "";
+    document.querySelector(".reset-btn-nav").classList.add("hidden");
+    document.querySelector(".resetUserName").classList.add("hidden");
     localStorage.removeItem("portfolio");
   }
 });
