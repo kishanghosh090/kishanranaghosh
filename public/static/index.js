4b5c66214e796b4e5ee0c1c8a65aeeb285ad3c05
kishanghosh090 <kishanghosh090@gmail.com>
2024-10-31 18:51:46 +0530

    project section modeify

--- public/static/index.js
+++ public/static/index.js
@@ -1,116 +1,108 @@
 // loader
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.loader').setAttribute('class', 'loader-hide');
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".loader").setAttribute("class", "loader-hide");
   // Audios
-const loading = new Audio("./audio/landing.mp3");
-const hamSound = new Audio("./audio/ham.wav");
-const contactSound = new Audio("./audio/contact.wav");
-const waterDrop = new Audio("./audio/waterDrop.mp3");
-const tok = new Audio("./audio/tok.mp3");
-const gotoTop = new Audio("./audio/GotoTOP.mp3");
-// variables
-const coverhide = document.querySelector(".cover-hide");
-const coverhide2 = document.querySelector(".cover-hide2");
-const hide = document.querySelector(".hide");
-const ham = document.querySelector(".ham");
-const hamani = document.querySelector(".ham-ani");
-const divrotate1 = document.querySelector(".div-rotate1");
-const divrotate2 = document.querySelector(".div-rotate2 ");
-const aboutme = document.querySelector(".aboutme")
-const home = document.querySelector(".home")
-const contactME = document.querySelector(".contactME")
-const linkedin = document.querySelector(".linkedin")
-const projecttoggle = document.querySelector(".project-toggle")
-const Projects = document.querySelector(".Projects")
-const crosshide = document.querySelector(".cross-hide")
-const crossProject = document.querySelector(".cross-project")
-const crosswlc = document.querySelector('.cross-wlc')
-const herocontact = document.querySelector(".hero-contact")
-const arrow = document.querySelector(".arrow")
-// use varibales
+  const loading = new Audio("./audio/landing.mp3");
+  const hamSound = new Audio("./audio/ham.wav");
+  const contactSound = new Audio("./audio/contact.wav");
+  const waterDrop = new Audio("./audio/waterDrop.mp3");
+  const tok = new Audio("./audio/tok.mp3");
+  const gotoTop = new Audio("./audio/GotoTOP.mp3");
+  // variables
+  const coverhide = document.querySelector(".cover-hide");
+  const coverhide2 = document.querySelector(".cover-hide2");
+  const hide = document.querySelector(".hide");
+  const ham = document.querySelector(".ham");
+  const hamani = document.querySelector(".ham-ani");
+  const divrotate1 = document.querySelector(".div-rotate1");
+  const divrotate2 = document.querySelector(".div-rotate2 ");
+  const aboutme = document.querySelector(".aboutme");
+  const home = document.querySelector(".home");
+  const contactME = document.querySelector(".contactME");
+  const linkedin = document.querySelector(".linkedin");
+  const projecttoggle = document.querySelector(".project-toggle");
+  const Projects = document.querySelector(".Projects");
+  const crosshide = document.querySelector(".cross-hide");
+  const crossProject = document.querySelector(".cross-project");
+  const crosswlc = document.querySelector(".cross-wlc");
+  const herocontact = document.querySelector(".hero-contact");
+  const arrow = document.querySelector(".arrow");
+  // use varibales
 
+  ham.addEventListener("click", () => {
+    hamSound.play();
+    coverhide.classList.toggle("cover-hide");
+    hide.classList.toggle("hide");
+    coverhide2.classList.toggle("cover-hide2");
+    hamani.classList.toggle("ham-ani");
+    divrotate1.classList.toggle("div-rotate1");
+    divrotate2.classList.toggle("div-rotate2");
+  });
+  home.addEventListener("click", () => {
+    tok.play();
+    coverhide.classList.toggle("cover-hide");
+    hide.classList.toggle("hide");
+    coverhide2.classList.toggle("cover-hide2");
+    hamani.classList.toggle("ham-ani");
+    divrotate1.classList.toggle("div-rotate1");
+    divrotate2.classList.toggle("div-rotate2");
+  });
+  aboutme.addEventListener("click", () => {
+    tok.play();
+    coverhide.classList.toggle("cover-hide");
+    hide.classList.toggle("hide");
+    coverhide2.classList.toggle("cover-hide2");
+    hamani.classList.toggle("ham-ani");
+    divrotate1.classList.toggle("div-rotate1");
+    divrotate2.classList.toggle("div-rotate2");
+  });
+  contactME.addEventListener("click", () => {
+    contactSound.play();
+    coverhide.classList.toggle("cover-hide");
+    hide.classList.toggle("hide");
+    coverhide2.classList.toggle("cover-hide2");
+    hamani.classList.toggle("ham-ani");
+    divrotate1.classList.toggle("div-rotate1");
+    divrotate2.classList.toggle("div-rotate2");
+  });
 
+  linkedin.addEventListener("click", () => {
+    coverhide.classList.toggle("cover-hide");
+    hide.classList.toggle("hide");
+    coverhide2.classList.toggle("cover-hide2");
+    hamani.classList.toggle("ham-ani");
+    divrotate1.classList.toggle("div-rotate1");
+    divrotate2.classList.toggle("div-rotate2");
+  });
 
+  Projects.addEventListener("click", () => {
+    waterDrop.play();
+    projecttoggle.classList.toggle("project-toggle");
+    crosshide.classList.toggle("cross-hide");
+    coverhide.classList.toggle("cover-hide");
+    hide.classList.toggle("hide");
+    coverhide2.classList.toggle("cover-hide2");
+    hamani.classList.toggle("ham-ani");
+    divrotate1.classList.toggle("div-rotate1");
+    divrotate2.classList.toggle("div-rotate2");
+  });
+  crosshide.addEventListener("click", () => {
+    projecttoggle.classList.toggle("project-toggle");
+    crosshide.classList.toggle("cross-hide");
+  });
+  crossProject.addEventListener("click", () => {
+    waterDrop.play();
+  });
+  herocontact.addEventListener("click", () => {
+    contactSound.play();
+  });
 
+  crosswlc.addEventListener("click", () => {
+    document.querySelector(".welcomeMsg").classList.remove("welcomeAnimation");
+    loading.play();
+  });
 
-
-
-
-ham.addEventListener("click", () => {
-  hamSound.play();
-  coverhide.classList.toggle("cover-hide");
-  hide.classList.toggle("hide");
-  coverhide2.classList.toggle("cover-hide2");
-  hamani.classList.toggle("ham-ani");
-  divrotate1.classList.toggle("div-rotate1");
-  divrotate2.classList.toggle("div-rotate2");
+  arrow.addEventListener("click", () => {
+    gotoTop.play();
+  });
 });
-home.addEventListener("click", () => {
-  tok.play();
-  coverhide.classList.toggle("cover-hide");
-  hide.classList.toggle("hide");
-  coverhide2.classList.toggle("cover-hide2");
-  hamani.classList.toggle("ham-ani");
-  divrotate1.classList.toggle("div-rotate1");
-  divrotate2.classList.toggle("div-rotate2");
-})
-aboutme.addEventListener("click", () => {
-  tok.play();
-  coverhide.classList.toggle("cover-hide");
-  hide.classList.toggle("hide");
-  coverhide2.classList.toggle("cover-hide2");
-  hamani.classList.toggle("ham-ani");
-  divrotate1.classList.toggle("div-rotate1");
-  divrotate2.classList.toggle("div-rotate2");
-})
-contactME.addEventListener("click", () => {
-  contactSound.play();
-  coverhide.classList.toggle("cover-hide");
-  hide.classList.toggle("hide");
-  coverhide2.classList.toggle("cover-hide2");
-  hamani.classList.toggle("ham-ani");
-  divrotate1.classList.toggle("div-rotate1");
-  divrotate2.classList.toggle("div-rotate2");
-})
-
-linkedin.addEventListener("click", () => {
-  coverhide.classList.toggle("cover-hide");
-  hide.classList.toggle("hide");
-  coverhide2.classList.toggle("cover-hide2");
-  hamani.classList.toggle("ham-ani");
-  divrotate1.classList.toggle("div-rotate1");
-  divrotate2.classList.toggle("div-rotate2");
-})
-
-Projects.addEventListener("click", () => {
-  waterDrop.play();
-  projecttoggle.classList.toggle("project-toggle");
-  crosshide.classList.toggle("cross-hide")
-  coverhide.classList.toggle("cover-hide");
-  hide.classList.toggle("hide");
-  coverhide2.classList.toggle("cover-hide2");
-  hamani.classList.toggle("ham-ani");
-  divrotate1.classList.toggle("div-rotate1");
-  divrotate2.classList.toggle("div-rotate2");
-})
-crosshide.addEventListener("click", () => {
-  projecttoggle.classList.toggle("project-toggle")
-  crosshide.classList.toggle("cross-hide")
-})
-crossProject.addEventListener("click", () => {
-  waterDrop.play();
-})
-herocontact.addEventListener("click", () => {
-  contactSound.play();
-})
-
-crosswlc.addEventListener('click', () => {
-  document.querySelector('.welcomeMsg').classList.remove('welcomeAnimation')
-  loading.play();
-})
-
-arrow.addEventListener('click', () => {
-  gotoTop.play();
-})
-
-})
