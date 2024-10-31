b95c5c675d003fe68cd18c6b324f3df3739148d5
kishanghosh090 <kishanghosh090@gmail.com>
2024-10-31 18:10:43 +0530

    project section modeify

--- public/script.js
+++ public/script.js
@@ -1,28 +1,31 @@
 const audio = new Audio("./audio/audio.mp3");
-document.getElementById('contactForm').addEventListener('submit', function (event) {
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    document.querySelector(".contactLoader").classList.remove("hidden");
     event.preventDefault();
-
     const formData = new FormData(this);
     const data = Object.fromEntries(formData.entries());
-
-    fetch('https://kishanranaghosh.onrender.com/send-email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-
-    }).then((response) => {
-        if (!response.ok) throw new Error('Network response was not ok');
-        return response.text();
+    fetch("https://kishanranaghosh.onrender.com/send-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-        .then((data) => {
-            document.getElementById('responseMessage').innerText = 'Message sent successfully!';
-            audio.play();
-            document.getElementById('contactForm').reset();
-        })
-        .catch((error) => {
-            console.log('There was a problem with the fetch operation:', error);
-        })
+      .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok");
 
-});
+        return response.text();
+      })
+      .then((data) => {
+        document.getElementById("responseMessage").innerText =
+          "Message sent successfully!";
+        audio.play();
+        document.querySelector(".contactLoader").classList.add("hidden");
+        document.getElementById("contactForm").reset();
+      })
+      .catch((error) => {
+        console.log("There was a problem with the fetch operation:", error);
+      });
+  });
